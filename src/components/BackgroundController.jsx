912200f8b66f12babb8b9977e0732a1ba2e4acbf
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-05-15 09:34:08 +0200

    changes

--- src/components/BackgroundController.jsx
+++ src/components/BackgroundController.jsx
@@ -1,6 +1,27 @@
+import { useState } from "react"
+import { Slider } from "./ui/slider"
+import ColorPickerController from "./ColorPickerController"
+
 const BackgroundController = () => {
+  const [rounded,setRounded] = useState(0)
+  const [padding,setPadding] = useState(0)
+  // eslint-disable-next-line no-unused-vars
+  const [color,setColor] = useState('#000')
   return (
-    <div>BackgrroundController</div>
+    <div>
+      <div className="p-2">
+          <label className="p-2 flex justify-between items-center">Rounded <span>{rounded} px</span> </label>
+          <Slider defaultValue={[0]} max={512} step={1} onValueChange={(event) => setRounded(event[0])} />
+        </div>
+      <div className="p-2">
+          <label className="p-2 flex justify-between items-center">Padding <span>{padding} px</span> </label>
+          <Slider defaultValue={[40]} max={100} step={1} onValueChange={(event) => setPadding(event[0])} />
+        </div>
+        <div className="p-2">
+          <label className="p-2 flex justify-between items-center">Icon color</label>
+          <ColorPickerController hideController={false} selectedColor={(color) => setColor(color)} />
+        </div>
+    </div>
   )
 }
 
