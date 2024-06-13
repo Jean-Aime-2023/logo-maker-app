aa46c41df76fc2a2874a610132644cba940544ae
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-06-13 21:48:21 +0200

    updates

--- src/components/IconController.jsx
+++ src/components/IconController.jsx
@@ -16,13 +16,11 @@ const IconController = () => {
     storageValue = {};
   }
 
-  // eslint-disable-next-line no-unused-vars
-  const [size, setSize] = useState(storageValue?storageValue?.iconsSize:280)
-  const [rotate, setRotate] = useState(storageValue?storageValue?.iconRotate:0)
-  // eslint-disable-next-line no-unused-vars
-  const [color, setColor] = useState(storageValue?storageValue?.iconColor:'#fff')
-  
-  const {updateStorage,setUpdateStorage} = useContext(UpdateStorageContext)
+  const [size, setSize] = useState(storageValue ? storageValue.iconsSize : 280)
+  const [rotate, setRotate] = useState(storageValue ? storageValue.iconRotate : 0)
+  const [color, setColor] = useState(storageValue ? storageValue.iconColor : '#fff')
+  const [icon, setIcon] = useState(storageValue ? storageValue.icon : 'Smile')
+  const { updateStorage, setUpdateStorage } = useContext(UpdateStorageContext)
 
   useEffect(() => {
     const updatedValue = {
@@ -30,32 +28,32 @@ const IconController = () => {
       iconsSize: size,
       iconRotate: rotate,
       iconColor: color,
-      icon: 'Smile'
+      icon: icon
     }
     setUpdateStorage(updatedValue)
     localStorage.setItem('value', JSON.stringify(updatedValue))
-  }, [size, rotate, color, storageValue])
+  }, [size, rotate, color, storageValue, icon])
 
-  
+
   return (
     <div>
       <div>
-        <IconList/>
+        <IconList selectedIcon={(icon) => setIcon(icon)} />
         <div className="p-2">
           <label className="p-2 flex justify-between items-center">Size <span>{size} px</span> </label>
           <Slider defaultValue={[size]} max={512} step={1} onValueChange={(event) => setSize(event[0])} />
         </div>
         <div className="p-2">
-          <label className="p-2 flex justify-between items-center">Rotate <span>{rotate} ° </span> </label>
+          <label className="p-2 flex justify-between items-center">Rotate <span>{rotate} deg</span> </label>
           <Slider defaultValue={[rotate]} max={360} step={1} onValueChange={(event) => setRotate(event[0])} />
         </div>
         <div className="p-2">
-          <label className="p-2 flex justify-between items-center">Icon color</label>
-          <ColorPickerController hideController={true} selectedColor={(color) => setColor(color)} />
+          <label className="p-2 flex justify-between items-center">Color <span>{color}</span></label>
+          <ColorPickerController color={color} setColor={setColor} />
         </div>
       </div>
     </div>
   )
 }
 
-export default IconController
+export default IconController;
--- src/components/IconList.jsx
+++ src/components/IconList.jsx
@@ -8,36 +8,193 @@ import {
     DialogHeader,
     DialogTitle,
 } from "@/components/ui/dialog"
-import { iconList } from "@/constants/icons"
-import { AlignHorizontalDistributeCenterIcon, Smile, icons } from "lucide-react"
 import { useState } from "react"
+import {
+    Smile,
+    AlignHorizontalDistributeCenterIcon,
+    AlignHorizontalDistributeEndIcon,
+    AlignHorizontalDistributeStartIcon,
+    AlignHorizontalJustifyEndIcon,
+    AlignHorizontalJustifyStartIcon,
+    AlignHorizontalSpaceAroundIcon,
+    AlignHorizontalSpaceBetweenIcon,
+    AlignVerticalDistributeCenterIcon,
+    AlignVerticalDistributeEndIcon,
+    AlignVerticalDistributeStartIcon,
+    AlignVerticalJustifyCenterIcon,
+    AlignVerticalJustifyStartIcon,
+    AlignVerticalSpaceAroundIcon,
+    AlignVerticalSpaceBetweenIcon,
+    ArrowDownRightFromCircleIcon,
+    ArrowDownRightFromSquareIcon,
+    AxeIcon,
+    BanIcon,
+    BedIcon,
+    BotIcon,
+    BoxIcon,
+    BugIcon,
+    BusIcon,
+    CogIcon,
+    CpuIcon,
+    DnaIcon,
+    DogIcon,
+    DotIcon,
+    EarIcon,
+    EggIcon,
+    EyeIcon,
+    FanIcon,
+    GemIcon,
+    GitPullRequestCreateArrowIcon,
+    HamIcon,
+    HopIcon,
+    KeyIcon,
+    LucideInspectionPanel,
+    LucidePhoneIncoming,
+    MapIcon,
+    MehIcon,
+    MicIcon,
+    MousePointerSquareDashedIcon,
+    NfcIcon,
+    PiIcon,
+    ScissorsSquareDashedBottomIcon,
+    SquareBottomDashedScissorsIcon,
+    SquareDashedMousePointerIcon,
+    TvIcon,
+} from "lucide-react";
 
 
+const icons = {
+    AlignHorizontalDistributeCenterIcon,
+    AlignHorizontalDistributeEndIcon,
+    AlignHorizontalDistributeStartIcon,
+    AlignHorizontalJustifyEndIcon,
+    AlignHorizontalJustifyStartIcon,
+    AlignHorizontalSpaceAroundIcon,
+    AlignHorizontalSpaceBetweenIcon,
+    AlignVerticalDistributeCenterIcon,
+    AlignVerticalDistributeEndIcon,
+    AlignVerticalDistributeStartIcon,
+    AlignVerticalJustifyCenterIcon,
+    AlignVerticalJustifyStartIcon,
+    AlignVerticalSpaceAroundIcon,
+    AlignVerticalSpaceBetweenIcon,
+    ArrowDownRightFromCircleIcon,
+    ArrowDownRightFromSquareIcon,
+    AxeIcon,
+    BanIcon,
+    BedIcon,
+    BotIcon,
+    BoxIcon,
+    BugIcon,
+    BusIcon,
+    CogIcon,
+    CpuIcon,
+    DnaIcon,
+    DogIcon,
+    DotIcon,
+    EarIcon,
+    EggIcon,
+    EyeIcon,
+    FanIcon,
+    GemIcon,
+    GitPullRequestCreateArrowIcon,
+    HamIcon,
+    HopIcon,
+    KeyIcon,
+    LucideInspectionPanel,
+    LucidePhoneIncoming,
+    MapIcon,
+    MehIcon,
+    MicIcon,
+    MousePointerSquareDashedIcon,
+    NfcIcon,
+    PiIcon,
+    ScissorsSquareDashedBottomIcon,
+    SquareBottomDashedScissorsIcon,
+    SquareDashedMousePointerIcon,
+    TvIcon,
+};
 
-const IconList = () => {
-    const [openDialog, setOpenDialog] = useState(false)
-    const Icon = ({ name, color, size }) => {
-        const LucidIcon = icons[name]
-        if (!LucidIcon) {
-            return;
-        }
-        return <LucidIcon color={color} size={size} />
+
+const CustomIcon = ({ name, color, size }) => {
+    const LucidIcon = icons[name];
+    if (!LucidIcon) {
+        return null;
     }
+    return <LucidIcon color={color} size={size} />;
+}
+
+const IconList = ({ selectedIcon }) => {
+    const iconList = [
+        "AlignHorizontalDistributeCenterIcon",
+        "AlignHorizontalDistributeStartIcon",
+        "AlignHorizontalDistributeEndIcon",
+        "AlignHorizontalJustifyEndIcon",
+        "AlignHorizontalJustifyStartIcon",
+        "AlignHorizontalSpaceAroundIcon",
+        "AlignHorizontalSpaceBetweenIcon",
+        "AlignVerticalDistributeCenterIcon",
+        "AlignVerticalDistributeEndIcon",
+        "AlignVerticalDistributeStartIcon",
+        "AlignVerticalJustifyCenterIcon",
+        "AlignVerticalJustifyStartIcon",
+        "AlignVerticalSpaceAroundIcon",
+        "AlignVerticalSpaceBetweenIcon",
+        "ArrowDownRightFromCircleIcon",
+        "ArrowDownRightFromSquareIcon",
+        "AxeIcon",
+        "BanIcon",
+        "BedIcon",
+        "BotIcon",
+        "BoxIcon",
+        "BugIcon",
+        "BusIcon",
+        "CogIcon",
+        "CpuIcon",
+        "DnaIcon",
+        "DogIcon",
+        "DotIcon",
+        "EarIcon",
+        "EggIcon",
+        "EyeIcon",
+        "FanIcon",
+        "GemIcon",
+        "GitPullRequestCreateArrowIcon",
+        "HamIcon",
+        "HopIcon",
+        "KeyIcon",
+        "LucideInspectionPanel",
+        "LucidePhoneIncoming",
+        "MapIcon",
+        "MehIcon",
+        "MicIcon",
+        "MousePointerSquareDashedIcon",
+        "NfcIcon",
+        "PiIcon",
+        "ScissorsSquareDashedBottomIcon",
+        "SquareBottomDashedScissorsIcon",
+        "SquareDashedMousePointerIcon",
+        "TvIcon",
+    ];
+
+    const [openDialog, setOpenDialog] = useState(false);
+
     return (
         <div>
             <div>
                 <label>Icon</label>
                 <div onClick={() => setOpenDialog(true)} className="p-3 cursor-pointer bg-gray-200 rounded-md w-[50px] h-[50px] flex items-center justify-center my-2"><Smile /></div>
             </div>
-            <Dialog open={openDialog}>
+            <Dialog open={openDialog} onOpenChange={setOpenDialog}>
                 <DialogContent>
                     <DialogHeader>
                         <DialogTitle>Pick Your Favorite Icon</DialogTitle>
                         <DialogDescription>
-                            <div>
+                            <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 overflow-auto h-[400px] p-6">
                                 {iconList.map((icon, index) => (
-                                    <div key={index}>
-                                       <Icon name={icon} color={'#000'} size={20} />
+                                    <div className="border p-3 flex rounded-sm items-center justify-center cursor-pointer" 
+                                    onClick={() => { selectedIcon(icon); setOpenDialog(false) }} key={index}>
+                                        <CustomIcon name={icon} color={'#000'} size={20} />
                                     </div>
                                 ))}
                             </div>
@@ -45,9 +202,8 @@ const IconList = () => {
                     </DialogHeader>
                 </DialogContent>
             </Dialog>
-
         </div>
-    )
+    );
 }
 
-export default IconList
+export default IconList;
